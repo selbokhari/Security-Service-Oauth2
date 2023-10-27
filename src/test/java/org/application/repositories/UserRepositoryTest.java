0062664c6685ca837d984ecfd56fd0784f4455dd
EL BOKHARI SOHAIB <selbokhari@sqli.com>
2023-10-27 03:42:43 +0100

    Test Unitaire pour la MAJ d'un utilisateur

--- src/test/java/org/application/repositories/UserRepositoryTest.java
+++ src/test/java/org/application/repositories/UserRepositoryTest.java
@@ -18,18 +18,30 @@ public class UserRepositoryTest {
     private UtilisateurEntite utilisateurEntite;
 
 
+    @BeforeEach
+    private void initialiserUtilisateurParDefaut() {
+        utilisateurEntite = UtilisateurEntite.builder()
+                .login(LOGIN_TEST)
+                .email("email@gmail.fr")
+                .password("password")
+                .nom("nom01")
+                .prenom("prenom01")
+                .build();
+    }
+
     @Test
     @DisplayName("Tester la persistance d'un utilisateur")
     public void saveUserEntiteTest() {
 
         // init: enregistrer un utilisateur pour effectuer le test => déjà faite sur @BeforeEach
 
-        // Action: récupérer l'utilisateur créer par son login
+        // action: récupérer l'utilisateur créer par son login
         UtilisateurEntite nouveauUtilisateur = userRepository.save(utilisateurEntite);
 
-        // Vérification: vérifier l'utilisateur récupéré
+        // vérification:  d'utilisateur récupéré
         Assertions.assertThat(nouveauUtilisateur).isNotNull();
         Assertions.assertThat(nouveauUtilisateur.getUserId()).isNotNull();
+        Assertions.assertThat(nouveauUtilisateur.getUserId()).isGreaterThan(0);
         Assertions.assertThat(nouveauUtilisateur.getLogin()).isEqualTo(utilisateurEntite.getLogin());
         Assertions.assertThat(nouveauUtilisateur.getEmail()).isEqualTo(utilisateurEntite.getEmail());
     }
@@ -41,24 +53,38 @@ public class UserRepositoryTest {
         // init : persister un utilisteur dans la BDD
         UtilisateurEntite nouveauUtilisateur = userRepository.save(utilisateurEntite);
 
-        // Action: récupérer l'utilisateur depuis la BDD par son login
+        // action: récupérer l'utilisateur depuis la BDD par son login
         UtilisateurEntite utilisateurRecupere = userRepository.findByLogin(LOGIN_TEST);
 
-        // Vérification: vérifier si c'est le bon utilisateur
+        // vérification: si c'est le bon utilisateur
         Assertions.assertThat(utilisateurRecupere).isNotNull();
         Assertions.assertThat(utilisateurRecupere.getUserId()).isNotNull();
         Assertions.assertThat(utilisateurRecupere.getLogin()).isEqualTo(LOGIN_TEST);
     }
 
-    @BeforeEach
-    private void initialiserUtilisateurParDefaut() {
-        utilisateurEntite = UtilisateurEntite.builder()
-                .login(LOGIN_TEST)
-                .email("email@gmail.fr")
-                .password("password")
-                .nom("nom01")
-                .prenom("prenom01")
-                .build();
+    @Test
+    @DisplayName("Tester la mise à jour d'un utilisateut")
+    public void miseAjourUtilisateurTest() {
+        final String nouveauEmail = "emailUpdate@gmail.fr";
+        final String nouveauNom = "nouveau nom";
+        final String nouveauPrenom = "nouveau prenom";
+        final String noveauLogin = "nouveau login";
+
+        // init: persister un utilisateur
+        UtilisateurEntite nouveauUtilisateur = userRepository.save(utilisateurEntite);
+        nouveauUtilisateur.setEmail(nouveauEmail);
+        nouveauUtilisateur.setNom(nouveauNom);
+        nouveauUtilisateur.setPrenom(nouveauPrenom);
+        nouveauUtilisateur.setLogin(noveauLogin);
+        // action: modifier l'utilisateur
+        UtilisateurEntite updatedUser = userRepository.save(nouveauUtilisateur);
+
+        // vérification: si les modifications ont ete prise en compte
+        Assertions.assertThat(updatedUser.getUserId()).isEqualTo(nouveauUtilisateur.getUserId());
+        Assertions.assertThat(updatedUser.getEmail()).isEqualTo(nouveauEmail);
+        Assertions.assertThat(updatedUser.getNom()).isEqualTo(nouveauNom);
+        Assertions.assertThat(updatedUser.getPrenom()).isEqualTo(nouveauPrenom);
+        Assertions.assertThat(updatedUser.getLogin()).isEqualTo(noveauLogin);
     }
 
 }
